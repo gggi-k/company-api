e3891be06b8c039715b8fca2cd4e1176dd0fca4a
jgram <jprgr16@gmail.com>
2022-11-17 12:27:59 +0900

    npm init

---
+++ src/main/java/kr/kko/kakaoassignapi/KakaoAssignApiApplication.java
@@ -0,0 +1,13 @@
+package kr.kko.kakaoassignapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoAssignApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaoAssignApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/kko/kakaoassignapi/KakaoAssignApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kko.kakaoassignapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoAssignApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
